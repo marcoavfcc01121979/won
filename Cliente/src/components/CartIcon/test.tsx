b182deb84055219493817e8a885c0633163baa86
Marco Ferreira <marcoavfcc@gmail.com>
2021-01-24 15:57:03 -0300

    working

---
+++ Cliente/src/components/CartIcon/test.tsx
@@ -0,0 +1,20 @@
+import { getByText, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import CartIcon from '.'
+
+describe('<CartIcon />', () => {
+  it('should render without badge', () => {
+    renderWithTheme(<CartIcon />)
+
+    expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
+    expect(screen.queryByLabelText(/cart items/i)).not.toBeInTheDocument()
+  })
+
+  it('should render with badge', () => {
+    renderWithTheme(<CartIcon quantity={3} />)
+
+    expect(screen.getByLabelText(/cart items/i)).toBeInTheDocument()
+    expect(screen.getByText(/3/)).toBeInTheDocument()
+  })
+})
