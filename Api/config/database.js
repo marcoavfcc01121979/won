82380c7cbeb0b1f64506e2e3fb1b76657391e246
Marco Ferreira <marcoavfcc@gmail.com>
2021-01-30 16:12:49 -0300

    update strapi

--- Api/config/database.js
+++ Api/config/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'wongames'),
-        username: env('DATABASE_USERNAME', 'wongames'),
+        username: env('DATABASE_USERNAME', 'postgres'),
         password: env('DATABASE_PASSWORD', 'root'),
         ssl: env.bool('DATABASE_SSL', false),
       },
