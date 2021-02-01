0b6bacb2e9a1abd99c7a6d2dd05f9cb016dc9216
Marco Ferreira <marcoavfcc@gmail.com>
2021-02-01 16:57:42 -0300

    working with Apollo

--- Cliente/src/pages/index.tsx
+++ Cliente/src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { gql, useQuery } from '@apollo/client';
+
 import Home, { HomeTemplateProps } from 'templates/Home'
 
 import bannerMocks from 'components/BannerSlider/mock'
@@ -5,12 +7,33 @@ import gamesMock from 'components/GameCardSlider/mock'
 import hightlightMock from 'components/Highlight/mock'
 
 export default function Index(props: HomeTemplateProps) {
+  const { data, loading, error } = useQuery(gql`
+    query getGames {
+      games {
+        name
+      }
+    }
+  `)
+
+  if(loading) return <p>Loading....</p>
+  if(error) return <p>{error}</p>
+  if(data) return <p>{JSON.stringify(data, null, 2)}</p>
+  // client.query({
+  //  query: gql`
+  //    query getGames {
+  //      games {
+  //        name
+  //      }
+  //    }
+  //  `
+  //})
+
   return <Home {...props} />
 }
 
-//getStaticProps  => gerar um estático em build time
-//getServerSideProps => gerar via ssr a cada request
-//getInitialProps => gerar via ssr a cada request
+//getStaticProps  => gerar um estático em build time { gatsby }
+//getServerSideProps => gerar via ssr a cada request { Nunca vai para o bundle do client }
+//getInitialProps => gerar via ssr a cada request { vai para o client, faz hydrate do lado do client depois do 1 req }
 export function getServerSideProps() {
   //faz lógica
   //pode ser busca dados numa API
