d8568640685e54f62b8fc2355fd37a2015a3236c
Marco Ferreira <marcoavfcc@gmail.com>
2021-02-03 18:52:44 -0300

    initial slug

--- Cliente/src/pages/games.tsx
+++ Cliente/src/pages/games.tsx
@@ -1,8 +1,11 @@
-import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
-import filterItemsMock from 'components/ExploreSidebar/mock'
+
 //import gamesMock from 'components/GameCardSlider/mock'
 import { initializeApollo } from 'utils/apollo'
-import { gql } from '@apollo/client'
+import { QUERY_GAMES } from 'graphql/queries/games'
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
+
+import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
+import filterItemsMock from 'components/ExploreSidebar/mock'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
@@ -11,22 +14,9 @@ export default function GamesPage(props: GamesTemplateProps) {
 export async function getStaticProps() {
   const apolloClient = initializeApollo()
 
-  const { data } = await apolloClient.query({
-    query: gql`
-      query QueryGames {
-        games {
-          name
-          slug
-          cover {
-            url
-          }
-          developers {
-            name
-          }
-          price
-        }
-      }
-    `
+  const { data } = await apolloClient.query<QueryGames, QueryGamesVariables>({
+    query: QUERY_GAMES,
+    variables: { limit: 9 }
   })
 
 
@@ -36,7 +26,7 @@ export async function getStaticProps() {
       games: data.games.map((game) => ({
         title: game.name,
         developer: game.developers[0].name,
-        img: `http://localhost:1337${game.cover?.url}`,
+        img: `http://localhost:1337${game.cover!.url}`,
         price: new Intl.NumberFormat('en', { style: 'currency', currency: 'USD' }).format(game.price)
       })),
       filterItems: filterItemsMock
